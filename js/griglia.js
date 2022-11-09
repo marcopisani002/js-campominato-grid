087247a59c77f310a78d71935851f6ccdf44497c
marcopisani002 <pisanimarco34@gmail.com>
2022-11-09 15:13:53 +0100

    fix bug on button generator

--- js/griglia.js
+++ js/griglia.js
@@ -6,7 +6,7 @@ btnInputGriglia.addEventListener("click", function () {
     const numCellInput = document.getElementById("numCelle");
 
 
-   revealGriglia(parseInt(numCellInput.value));
+    revealGriglia(parseInt(numCellInput.value));
 
 
 
@@ -16,30 +16,30 @@ btnInputGriglia.addEventListener("click", function () {
 function revealGriglia(numCelle) {
     const gridSection = document.querySelector(".grid-section");
 
-   
-    
+
+
     gridSection.innerHTML = "";
 
     const finalCells = Math.pow(numCelle, 2);
 
 
     for (let i = 0; i < finalCells; i++) {
-       
-      
+
+
         const newCell = document.createElement("div");
         newCell.classList.add("grid-cell");
         newCell.style.flexBasis = 100 / numCelle + "%";
         newCell.style.width = `calc(100% / ${numCelle})`;
-       
+
         newCell.addEventListener("click", function () {
-           
-             newCell.classList.toggle("bg-success");
-       
+
+            newCell.classList.toggle("bg-primary");
+            newCell.innerHTML = `<div class="text-white fw-bold"> ${i}</div> `;
         });
 
-       
-        gridSection.append(newCell,);
-        
+
+        gridSection.append(newCell);
+
     }
 }
 
